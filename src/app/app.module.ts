de0c435849cb26abb9fd425cfbf118c0207b5af7
rafaalvesf <37000965+rafaalvesf@users.noreply.github.com>
2019-11-24 22:44:33 -0300

    New repository
    Create a new repository

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenuComponent } from './menu/menu.component';
+import { ClientesComponent } from './clientes/clientes.component';
+import { ContatosComponent } from './contatos/contatos.component';
+
+const appRoutes: Routes = [
+  { path: 'menu', component: MenuComponent },
+  { path: 'clientes',      component: ClientesComponent },
+  { path: 'contatos',      component: ContatosComponent },
+];
 
 @NgModule({
   declarations: [
@@ -10,7 +21,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
